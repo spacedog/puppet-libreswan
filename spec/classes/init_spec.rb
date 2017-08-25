c20f32264be4e2a6d18a8c22ed32cece8107e692
Anton Baranov <abaranov@linuxfoundation.org>
2017-08-25 10:04:52 -0400

    Fix service class subscription target
    Signed-off-by: Anton Baranov <abaranov@linuxfoundation.org>

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -14,7 +14,7 @@ describe 'libreswan' do
           it { is_expected.to contain_class('libreswan::params') }
           it { is_expected.to contain_class('libreswan::install') }
           it { is_expected.to contain_class('libreswan::config').that_requires('Class[libreswan::install]') }
-          it { is_expected.to contain_class('libreswan::service').that_subscribes_to('libreswan::config') }
+          it { is_expected.to contain_class('libreswan::service').that_subscribes_to('Class[libreswan::config]') }
         end
       end
     end
